126dfc29cac21f4eacfc04e3b9e6a5d9e15968e5
LeandroWA <leandro.bro04@gmail.com>
2025-06-03 21:40:24 -0300

    inicio aula 01 js

---
+++ JS/JS01-Introducao/main.js
@@ -0,0 +1,33 @@
+var idade
+const ano = 2010
+
+// JavaScript
+console.log("Hello Word")
+console.clear()
+
+//Variaveis
+var numero1 = 192
+console.log(numero1)
+
+
+var num1 = 5
+var num2 = 4
+var resultado = num1 + num2
+console.log(resultado)
+
+
+idade = 89 // iniciar a variavel em oura parte do codigo
+console.log(idade)
+
+ano = 1936
+const PI = 3.14
+
+// Textos - Strings
+
+var nome = "leandro"
+var nome2 = "Leandro2"
+var nome3 = "716474.4674" //Textos
+
+console.log(nome)
+console.log(nome2)
+console.log(nome3)
