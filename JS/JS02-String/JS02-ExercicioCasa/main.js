7a7925f7b622e50e034ea18387151bebbd095eb7
LeandroWA <leandro.bro04@gmail.com>
2025-06-15 23:41:22 -0300

    exercicios

---
+++ JS/JS02-String/JS02-ExercicioCasa/main.js
@@ -0,0 +1,10 @@
+let frase = "Estamos aprendendo JavaScript e seremos futuros Devs";
+
+// Caracter na posição 19 (lembrando que começa do 0)
+console.log("O caracter que está na posição 19 é: " + frase.charAt(19));
+
+// Tamanho da string
+console.log("O tamanho de toda a String é: " + frase.length);
+
+// String toda em maiúsculo
+console.log("Toda a String em maiúsculo: " + frase.toUpperCase());
